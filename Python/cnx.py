903ccf6f2ef22dc33a8781c5f22f68c02d4d14b7
cdr3xl3r <126210495+cdr3xl3r@users.noreply.github.com>
2023-10-19 19:48:31 -0400

    update to cnx and DB test account

--- Python/cnx.py
+++ Python/cnx.py
@@ -2,17 +2,41 @@
  
 import mysql.connector
 from mysql.connector import errorcode
- 
-try:
-  cnx = mysql.connector.connect(user='root',
-                                password='bowl7173',
+
+
+
+print("\nLoading-cnx.py...") 
+try: #Try to establish a connection
+  print("cnx-TRYING...")
+  cnx = mysql.connector.connect(user='cita450',
+                                password='cita450',
                                 database='db_cita450')
-except mysql.connector.Error as err:
+  #print result
+  print("cnx-ACTIVE...")
+  cursor=cnx.cursor()
+  print("cnx-TEST-")
+  cursor.execute("SHOW TABLES")
+  for table in cursor:
+    print(table)
+  cursor.execute("select * from users as u \
+    join ledger as l on l.user_id = u.id \
+    join line as n on n.ledger_id = l.id \
+    order by l.id;")
+  for line in cursor:
+    print(line)
+  
+  
+except mysql.connector.Error as err:#error handling
   if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
     print("Something is wrong with your user name or password")
+    print("cnx-INACTIVE")
   elif err.errno == errorcode.ER_BAD_DB_ERROR:
     print("Database does not exist")
+    print("cnx-INACTIVE")
   else:
     print(err)
-else:
+    print("cnx-INACTIVE")
+else: #close connection when finished
+  print("cnx-KILL...")
   cnx.close()
+print("cnx-INACTIVE\n")
