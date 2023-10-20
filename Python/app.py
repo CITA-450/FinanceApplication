14b7c078a85f060f0abfe86c73def83066bcbde4
Donald Bowles <90214227+don-bowles4@users.noreply.github.com>
2023-10-20 19:44:46 -0400

    flask app with ass. html

--- Python/app.py
+++ Python/app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, render_template, redirect, url_for, session
-import cnx
+import mysql.connector
+
+#create MySQL connection
+cnx = mysql.connector.connect(host='127.0.0.1', port = '3306', user='root', password = 'bowl7173', database='db_cita450')
 
 app = Flask(__name__)
-app.secret_key = '123456'
 
+app.secret_key = 'secretkey12'
 
 @app.route('/')
 def home():
@@ -13,24 +16,24 @@ def home():
 
 @app.route('/login', methods=['POST'])
 def login():
-    username = request.form['email']
+    username = request.form['username']
     password = request.form['password']
 
-    cursor = db.cursor()
+    cursor = cnx.cursor()
     cursor.execute("SELECT * FROM users WHERE email=%s AND passwd=%s", (username, password))
     user = cursor.fetchone()
     cursor.close()
-
+    cnx.close()
     if user:
-        session['email'] = username
-        return redirect(url_for('home'))
+        session['username'] = username
+        return render_template('main.html')
     else:
         return 'Login failed. Invalid credentials.'
 
-@app.route('/logout')
-def logout():
-    session.pop('email', None)
-    return redirect(url_for('home'))
+#@app.route('/logout')
+#def logout():
+#    session.pop('email', None)
+#    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
